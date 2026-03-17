2e75100d4186444cf24ef99c573ed6b0237da641
Gustavo Delgado <gdquevedo@gmail.com>
2026-03-17 12:04:08 -0600

    Corrigiendo definición de usuario para Azure SQL

--- DB/Security/ManejoPresupuestoApp.sql
+++
@@ -1,2 +0,0 @@
-﻿CREATE USER [ManejoPresupuestoApp] FOR LOGIN [ManejoPresupuestoApp];
-
--- DB/Security/ManejoPresupuestoApp_1.sql
+++ DB/Security/ManejoPresupuestoApp_1.sql
@@ -1,3 +1,3 @@
-﻿CREATE LOGIN [ManejoPresupuestoApp]
-    WITH PASSWORD = N'gfngc|DeJhqjvNo7emn{h|jnmsFT7_&#$!~<zXhmcljgh|fo', SID = 0x8791E9143736594C9B3264A6AB11E501, DEFAULT_DATABASE = [ManejoPresupuesto], DEFAULT_LANGUAGE = [us_english], CHECK_POLICY = OFF;
+﻿CREATE USER [ManejoPresupuestoApp] 
+    WITH PASSWORD = 'gfngc|DeJhqjvNo7emn{h|jnmsFT7_&#$!~<zXhmcljgh|fo';
 
